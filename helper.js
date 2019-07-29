1d0ded21cd0b76e6ae13e3dd58f0644fd183ae87
Kumar Anirudha <anirudhastark@yahoo.com>
2019-07-29 17:38:26 +0530

    role based access implemented

--- helper.js
+++ helper.js
@@ -1,6 +1,11 @@
 const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 var utils = require('./utils');
+const path = require('path');
+
+const neode = require('neode')
+    .fromEnv()
+    .withDirectory(path.join(__dirname, 'models'));
 
 module.exports = {
 
@@ -11,33 +16,85 @@ module.exports = {
     },
 
     checkAuth: function (req, res, next) {
-        if ('authorization' in req.headers && req.headers.authorization.length > 0) {
-            utils.verifyAuthToken(req.headers.authorization, function (authErr, authorised) {
-                if(authErr) {
-                    module.exports.sendResponse({
-                        'success': false,
-                        'statusCode': 401,
-                        'data': null,
-                        'errors': authErr,
-                        'message': 'Unauthorized User!'
-                    }, res)
-                }
-                else {
-                    req.authorised = authorised;
-                    next();
-                }
-            })
-        }
-        else {
+        try {
+            if ('authorization' in req.headers && req.headers.authorization.length > 0) {
+                utils.verifyAuthToken(req.headers.authorization, function (authErr, authorised) {
+                    if(authErr) {
+                        console.log('authErr:', authErr);
+                        module.exports.sendResponse({
+                            'success': false,
+                            'statusCode': 401,
+                            'data': null,
+                            'errors': authErr,
+                            'message': 'Unauthorized User!'
+                        }, res)
+                    }
+                    else {
+                        req.authorised = authorised;
+                        next();
+                    }
+                })
+            }
+            else {
+                module.exports.sendResponse({
+                    'success': false,
+                    'statusCode': 401,
+                    'data': null,
+                    'errors': 'No Auth Token Found',
+                    'message': 'Please Login to continue!'
+                }, res)
+            }
+        } catch (e) {
+            console.log('error in checkAuth:', e);
             module.exports.sendResponse({
                 'success': false,
                 'statusCode': 401,
                 'data': null,
-                'errors': 'No Auth Token Found',
-                'message': 'Please Login to continue!'
+                'errors': 'Access Error',
+                'message': 'Something went wrong. Please try again later!'
             }, res)
         }
-    }
+    },
+
+    checkAccess: function (req, res, next) {
+        try {
+            let responseData = null;
+            // fetch user data.
+            neode.first('User', 'id', req.authorised)
+            .then(user => {
+                if (user.get('role') && user.get('role') == 'admin') {
+                    next();
+                }
+                else {
+                    module.exports.sendResponse({
+                        'success': false,
+                        'statusCode': 401,
+                        'data': null,
+                        'errors': 'Access Restricted',
+                        'message': 'You do not have required access for this endpoint. If you think this is a mistake please connect with admin at hello@solvio.org!'
+                    }, res)
+                }
+            })
+            .catch(() => {
+                module.exports.sendResponse({
+                    'success': true,
+                    'statusCode': 400,
+                    'data': null,
+                    'errors': 'User not found!',
+                    'message': 'Something went wrong while trying to fetch User! Please re-login if this issue persists!'
+                }, res)
+            })
+        } catch (e) {
+            console.log('e:', e);
+            module.exports.sendResponse({
+                'success': false,
+                'statusCode': 401,
+                'data': null,
+                'errors': e,
+                'message': 'Something went wrong while checking your access to this endpoint. Please try again later!'
+            }, res)
+        }
+    },
 
 }
 
